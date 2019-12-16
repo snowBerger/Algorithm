5f6fd360d244335a68a3e4db3d948504da92a789
wangzhenhua <wangzhenhua@icarbonx.com>
2019-12-16 13:56:36 +0800

    update

--- threeSum.js
+++ threeSum.js
@@ -3,17 +3,35 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-  nums = nums.sort()
   let res = []
-  let i, j, k
+  const length = nums.length
+  if (!nums || length < 3) return res
 
-  for (i = 0, l = nums.length; i < l; i++) {
-    j = i + 1
-    k = i + 2
-    if () {
+  nums.sort((a, b) => a - b)
 
+  for (i = 0; i < length; i++) {
+    if (nums[i] > 0) break
+    if (nums[i] === nums[i - 1]) continue
+    let L = i + 1
+    let R = length - 1
+    while (L < R) {
+      const sum = nums[i] + nums[L] + nums[R]
+
+      if (sum === 0) {
+        res.push([nums[i], nums[L], nums[R]])
+        while (L < R && nums[L] === nums[L + 1]) L++
+        while (L < R && nums[R] === nums[R - 1]) R--
+        L++
+        R--
+      } else if (sum > 0) {
+        R--
+      } else if (sum < 0) {
+        L++
+      }
     }
   }
+
+  return res
 };
 
 
